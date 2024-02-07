3f2608cfbbdeb777d00d61ef20ce235b76d68c3e
Evan <evan.wilkinson@redhat.com>
2024-02-06 21:24:32 -0500

    prebuild ts errors, build module found errors

--- stories/Support.stories.tsx
+++ stories/Support.stories.tsx
@@ -1,17 +1,17 @@
 import React, { ComponentProps } from 'react';
-import { Support } from '@app/Support/Support';
+import { PrimaryDetailCardView as Resources } from '@app/Resources/Resources';
 import { Story } from '@storybook/react';
 
 //👇 This default export determines where your story goes in the story list
 export default {
   title: 'Components/Support',
-  component: Support,
+  component: Resources,
 };
 
 //👇 We create a “template” of how args map to rendering
-const Template: Story<ComponentProps<typeof Support>> = (args) => <Support {...args} />;
+const Template: Story<ComponentProps<typeof Resources>> = (args) => <Resources {...args} />;
 
-export const SupportStory = Template.bind({});
-SupportStory.args = {
+export const ResourcesStory = Template.bind({});
+ResourcesStory.args = {
   /*👇 The args you need here will depend on your component */
 };
